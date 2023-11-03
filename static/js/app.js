2b41827338b9a0c6b513a762ace7a13f9886db51
Michaelgcariello <carim@udel.edu>
2023-11-03 18:05:29 -0400

    Create sample bar and bubble graph, update demographics

--- static/js/app.js
+++ static/js/app.js
@@ -1,46 +1,102 @@
 const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
-
-// Fetch the JSON data and console log it
-d3.json(url).then(function(data) {
-  console.log(data);
-});
+let dropdown = d3.select('#selDataset')
+let metaDataContainer = d3.select('#sample-metadata')
 
 // Making data variables
 d3.json(url).then(function(data){
     names = data.names;
-    metadata = data.metadata;
-    sample = data.samples;
-    orderedsamples = samples.sort((a,b) => b.sample_values - a.sample_values)
-    init()
+    metaData = data.metadata;
+    samples = data.samples;
+    sortedSamples = samples.sort((a,b) => b.sample_values - a.sample_values)
+    populateDropdown(names)
+    createBarGraph(sortedSamples[0])
+    createBubbleChart(sortedSamples[0])
+    createDemographicInfo(sortedSamples[0].id)
 });
 
-//Horizontal Bar Graph
-d3.json(url).then((data) => {
+function populateDropdown (names) {
+    names.forEach((id) => {
+        dropdown.append("option").text(id).property("value",id);
+    });
+}
 
-      let samples = data.samples;
-      let sample_values = valueData.sample_values;
-      let result = sampleInfo.filter(result => result.id == bar);
-      let value = value[0];
-      let otu_ids = valueData.otu_ids;
-      let otu_labels = valueData.otu_labels;
+function createBarGraph (sample) {
+    let yticks = sample.otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
+    let xticks = sample.sample_values.slice(0,10).reverse();
+    let labels = sample.otu_labels.slice(0,10).reverse();
+    
+    let trace = {
+        x: xticks,
+        y: yticks,
+        text: labels,
+        type: "bar",
+        orientation: "h"
+    };
 
-      console.log(otu_ids,otu_labels,sample_values);
+    Plotly.newPlot("bar", [trace])
+    
+}
 
-      let xticks = sample_values.slice(0,10).reverse();
-      let yticks = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
-      let labels = otu_labels.slice(0,10).reverse();
+function createBubbleChart(sample) {
+    let otuIds = sample.otu_ids
+    let sampleValues = sample.sample_values
+    let otuLabels = sample.otu_labels
+    
+    let trace = {
+        x: otuIds,
+        y: sampleValues,
+        text: otuLabels,
+        mode: "markers",
+        marker: {
+            size: sampleValues,
+            color: otuIds,
+            colorscale: "Earth"
+        }
+    };
+
+
+    Plotly.newPlot("bubble", [trace], {xaxis:{title:"OTU ID"}})
+}
+
+function createDemographicInfo (id) {
+    let sampleMetaData = metaData.filter((data)=>data.id == id)[0]
+    d3.select("#sample-metadata").html("")
+    Object.entries(sampleMetaData).map(([k,v])=>{
+        console.log(k,v)
+        d3.select("#sample-metadata").append("h5").text(`${k}: ${v}`);
+    })
+}
+
+function optionChanged (id) {
+    const subject = sortedSamples.filter((sample)=>sample.id === id)[0]
+    createBarGraph(subject)
+    createBubbleChart(subject)
+    createDemographicInfo(id)
+}
+
+// //Horizontal Bar Graph
+// d3.json(url).then((data) => {
+
+//       let samples = data.samples;
+//       let sample_values = valueData.sample_values;
+//       let result = sampleInfo.filter(result => result.id == bar);
+//       let value = value[0];
+//       let otu_ids = valueData.otu_ids;
+//       let otu_labels = valueData.otu_labels;
+
+//       console.log(otu_ids,otu_labels,sample_values);
+
+//       let xticks = sample_values.slice(0,10).reverse();
+//       let yticks = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
+//       let labels = otu_labels.slice(0,10).reverse();
       
-      let trace = {
-          x: xticks,
-          y: yticks,
-          text: labels,
-          type: "bar",
-          orientation: "h"
-      };
+//       let trace = {
+//           x: xticks,
+//           y: yticks,
+//           text: labels,
+//           type: "bar",
+//           orientation: "h"
+//       };
 
-      Plotly.newPlot("bar", [trace])
-  });
-};
-
-//Bubble Chart
-
+//       Plotly.newPlot("bar", [trace])
+//   });
